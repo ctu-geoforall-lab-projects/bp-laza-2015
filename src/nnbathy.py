fe37848024a3b72d7c9b413aeae2b604c925191d
Adam Laza <ad.laza32@gmail.cz>
2014-10-22 00:57:25 +0200

    First OOP attempt

--- src/nnbathy.py
+++ src/nnbathy.py
@@ -1,19 +1,14 @@
+import grass.script as grass
+
 class nnbathy:
     def __init__(self, name):
         self.name = name
-        self.temp = grass.tempfile
-        # ...
-    
-    def __del__(self):
-        # cleanup
-        
-    def compute():
-        # volat nnbathy
-        pass
+        # self.temp = grass.tempfile
+        self.TMPXYZ = grass.tempfile()
+        self.XYZout = grass.tempfile()
+        self.TMP = grass.tempfile()	
+        self.TMPcat = grass.tempfile()
 
-    def create_output():
-        # vytvori vystupni rastr
-        pass
 
     def region(self):
         reg = grass.read_command("g.region", flags='p')
@@ -37,35 +32,123 @@ class nnbathy:
         self.nn_e = reg_E - ewres/2
         self.null = "NaN"
         self.ctype = "double"
+    
+    def compute(self):
+        # volat nnbathy
+        grass.message('"nnbathy" is performing the interpolation now. This may take some time...')
+        grass.verbose("Once it completes an 'All done.' message will be printed.")
 
+        #nnbathy calling
+        fsock = open(self.XYZout, 'w')
+        #TODO zkontrolovat zarovnani
+        grass.call(['nnbathy',
+                    '-W', '%d' % 0,
+                    '-i', '%s' % self.TMPXYZ,
+                    '-x', '%d' % self.nn_w, '%d' % self.nn_e,
+                    '-y', '%d' % self.nn_n, '%d' % self.nn_s,
+                    '-P', '%s' % self.ALG,
+                    '-n', '%dx%d' % (self.cols, self.rows)],
+                   stdout=fsock)
+        fsock.close()
+
+
+    def create_output(self):
+        # vytvori vystupni rastr
+        # convert the X,Y,Z nnbathy output into a GRASS ASCII grid, then import with r.in.ascii
+        # 1 create header
+        header = open(self.TMP, 'w')
+        header.write('north: %s\nsouth: %s\neast: %s\nwest: %s\nrows: %s\ncols: %s\ntype: %s\nnull: %s\n\n' % \
+                         (self.nn_n, self.nn_s, self.nn_e,  self.nn_w, self.rows, self.cols, self.ctype, self.null))
+        header.close()
+
+        # 2 do the conversion
+        grass.message("Converting nnbathy output to GRASS raster ...")
+        fin = open(self.XYZout, 'r')
+        fout = open(self.TMP, 'a')
+        cur_col = 1
+        for line in fin:
+            parts = line.split(" ")
+            if cur_col == cols:
+                cur_col = 0
+                fout.write(str(parts[2]))
+            else:
+                fout.write(str(parts[2]).rstrip('\n')+' ')
+            cur_col += 1
+        fin.close()
+        fout.close()
+
+        # 3 import to raster
+        grass.run_command('r.in.ascii', input=self.TMP, output=options['output'], quiet=True)
+        pass
+
+    def __del__(self):
+        # cleanup
+        if self.TMP:
+            os.remove(self.TMP)
+        if self.TMPXYZ:
+            os.remove(self.TMPXYZ)
+        if self.XYZout:
+            os.remove(self.XYZout)    
+        if self.TMPcat:
+            os.remove(self.TMPcat)    
 
 class nnbathy_raster(nnbathy):
-    def __init__(self, name):
-         nnbathy.__init__(name)
+    #__init__(self, name):
+    def __init__(name):
         self._load()
 
-    def _load():
+    def _load(self):
         # nacte vstupni raster
         # r.out.ascii input=self.name
         self.region()
-        print self.null
-        pass
+        grass.run_command('r.stats', flags='1gn', input=self.name, output=self.TMPXYZ, quiet=True)
+        # print self.null
 
-class nnbathy_vector:
+class nnbathy_vector(nnbathy):
     def __init__(self, name):
         self._load()
 
-    def _load():
+    def _load(self):
         # nacte vstupni vektor
-        pass
+        self.region()
+
+        if int(options['layer']) == 0:
+            LAYER = ''
+            COLUMN = ''
+        else:
+            LAYER = int(options['layer'])
+            if options['zcolumn']:
+                COLUMN = options['zcolumn']
+            else:
+                grass.message('Name of z column required for 2D vector maps.')
+
+        if options['kwhere']:
+            grass.run_command("v.out.ascii", flags='r', overwrite=1, input=options['input'], output=TMPcat, \
+                              format="point", separator="space", dp=15, where=options['kwhere'], layer=LAYER, columns=COLUMN)
+        else:
+            grass.run_command("v.out.ascii", flags='r', overwrite=1, input=options['input'], output=TMPcat, \
+                              format="point", separator="space", dp=15, layer=LAYER, columns=COLUMN)
+
+        if int(options['layer']) > 0:
+            fin = open(TMPcat, 'r')
+            fout = open(TMPXYZ, 'w')
+            try:
+                for line in fin:
+                    parts = line.split(" ")
+                    fout.write(parts[0]+' '+parts[1]+' '+parts[3])
+            except StandardError, e:
+                grass.fatal_error("Invalid input: %s" % e)
+            fin.close()
+            fout.close()
+        else:
+            # TODO: chyba ?
+            grass.message("Z coordinates are used.")
+
 
 class nnbathy_file:
     def __init__(self, name):
         self._load()
 
-    def _load():
+    def _load(self):
         # nacte vstupni soubor
-        pass
-            
-
-    
+        self.TMPXYZ=self.name
--- src/v.surf.nnbathy.py
+++ src/v.surf.nnbathy.py
@@ -85,192 +85,32 @@ import sys
 from grass.script.core import parser
 import grass.script as grass
 
-# from nnbathy import nnbathy_vector, nnbathy_file
+from nnbathy import nnbathy_vector
 
 TMP = None
 TMPcat = None
 TMPXYZ = None
 XYZout = None
 
-# def main():
-#     if vector: 
-#         obj = nnbathy_vector(nazev vektor)
-#     else:
-#         obj = nnbathy_file(cesta k souboru)
-
-#     obj.compute()
-#     obj.create_output()
-
 def main():
-    def region():
-        # set the region
-        # TODO globalni promenne?
-        global area, ALG, nn_n, nn_s, nn_w, nn_e, null, ctype, cols, rows
-        reg = grass.read_command("g.region", flags='p')
-        kv = grass.parse_key_val(reg, sep=':')
-        reg_N = float(kv['north'])
-        reg_W = float(kv['west'])
-        reg_S = float(kv['south'])
-        reg_E = float(kv['east'])
-        cols = int(kv['cols'])
-        rows = int(kv['rows'])
-        nsres = float(kv['nsres'])
-        ewres = float(kv['ewres'])
-        reg = (reg_N, reg_W, reg_S, reg_E)
-        area = (reg_N-reg_S)*(reg_E-reg_W)
-        ALG = options['algorithm']
+    if (options['input'] and options['file']):
+        grass.fatal("Please specify either the 'input' or 'file' option, not both.")
 
-        # set the working region for nnbathy (it's cell-center oriented)
-        nn_n = reg_N - nsres/2
-        nn_s = reg_S + nsres/2
-        nn_w = reg_W + ewres/2
-        nn_e = reg_E - ewres/2
-        null = "NaN"
-        ctype = "double"
+    if not(options['input'] or options['file']):
+        grass.message("Please specify either the 'input' or 'file' option.")
 
-    def initial_controls():
-        # setup temporary files
-        global TMP, TMPcat, TMPXYZ, XYZout
-        TMP = grass.tempfile()
-        TMPcat = grass.tempfile()
-        TMPXYZ = grass.tempfile()
-        XYZout = grass.tempfile()
+    if (options['file'] and os.path.isfile(options['file'])):
+        grass.message("File "+options['file']+" does not exist.")
 
-        if (TMPcat or TMPXYZ or XYZout or TMP) is None:
-            grass.fatal("Unable to create temporary files.")
-
-        # other controls
-        if not grass.find_program('nnbathy'):
-            grass.fatal('nnbathy is not available')
-
-        if (options['input'] and options['file']):
-            grass.message("Please specify either the 'input' or 'file' option, not both.")
-
-        if not(options['input'] or options['file']):
-            grass.message("Please specify either the 'input' or 'file' option.")
-
-        if (options['file'] and os.path.isfile(options['file'])):
-            grass.message("File "+options['file']+" does not exist.")
-
-        if area == 0:
-            grass.fatal(_("xy-locations are not supported"))
-            grass.fatal(_("Need projected data with grids in meters"))
-
-        if not options['file']:
-            if int(options['layer']) == 0:
-                LAYER = ''
-                COLUMN = ''
-            else:
-                LAYER = int(options['layer'])
-                if options['zcolumn']:
-                    COLUMN = options['zcolumn']
-                else:
-                    grass.message('Name of z column required for 2D vector maps.')
-
-            if options['kwhere']:
-                grass.run_command("v.out.ascii", flags='r', overwrite=1, input=options['input'], output=TMPcat,
-                                  format="point", separator="space", dp=15, where=options['kwhere'], layer=LAYER, columns=COLUMN)
-            else:
-                grass.run_command("v.out.ascii", flags='r', overwrite=1, input=options['input'], output=TMPcat,
-                                  format="point", separator="space", dp=15, layer=LAYER, columns=COLUMN)
-
-            if int(options['layer']) > 0:
-                fin = open(TMPcat, 'r')
-                fout = open(TMPXYZ, 'w')
-                try:
-                    for line in fin:
-                        parts = line.split(" ")
-                        fout.write(parts[0]+' '+parts[1]+' '+parts[3])
-                except StandardError, e:
-                    grass.fatal_error("Invalid input: %s" % e)
-                fin.close()
-                fout.close()
-            else:
-                # TODO: chyba ?
-                grass.message("Z coordinates are used.")
-        else:
-            TMPXYZ = options['file']
-
-    def compute():
-        grass.message('"nnbathy" is performing the interpolation now. This may take some time...')
-        grass.verbose("Once it completes an 'All done.' message will be printed.")
-
-        #nnbathy calling
-        fsock = open(XYZout, 'w')
-        #TODO zkontrolovat zarovnani
-        grass.call(['nnbathy',
-                    '-W', '%d' % 0,
-                    '-i', '%s' % TMPXYZ,
-                    '-x', '%d' % nn_w, '%d' % nn_e,
-                    '-y', '%d' % nn_n, '%d' % nn_s,
-                    '-P', '%s' % ALG,
-                    '-n', '%dx%d' % (cols, rows)],
-                   stdout=fsock)
-        fsock.close()
-
-        #TODO
-        # Y in "r.stats -1gn" output is in descending order, thus -y must be in
-        # MAX MIN order, not MIN MAX, for nnbathy not to produce a grid upside-down
-
-    def convert():
-        # convert the X,Y,Z nnbathy output into a GRASS ASCII grid, then import with r.in.ascii
-        # 1 create header
-        header = open(TMP, 'w')
-        header.write('north: %s\nsouth: %s\neast: %s\nwest: %s\nrows: %s\ncols: %s\ntype: %s\nnull: %s\n\n' % \
-                         (nn_n, nn_s, nn_e,  nn_w, rows, cols, ctype, null))
-        header.close()
-
-        # 2 do the conversion
-        grass.message("Converting nnbathy output to GRASS raster ...")
-        fin = open(XYZout, 'r')
-        fout = open(TMP, 'a')
-        cur_col = 1
-        for line in fin:
-            parts = line.split(" ")
-            if cur_col == cols:
-                cur_col = 0
-                fout.write(str(parts[2]))
-            else:
-                fout.write(str(parts[2]).rstrip('\n')+' ')
-            cur_col += 1
-        fin.close()
-        fout.close()
-
-    def import_to_raster():
-        grass.run_command('r.in.ascii', input=TMP, output=options['output'], quiet=True)
-
-        # store comand history in raster's metadata
-        # TODO formatovani
-        if options['input']:
-            grass.run_command('r.support', map=options['output'],
-                              history="v.surf.nnbathy alg=%s input=%s output=%s" % \
-                                  (options['algorithm'], options['input'], options['output']))
-        else:
-            grass.run_command('r.support', map=options['output'],
-                              history="v.surf.nnbathy alg=%s input=%s output=%s" % \
-                                  (options['algorithm'], options['file'], options['output']))
-
-        grass.run_command('r.support', map=options['output'],
-                          history="\nnnbathy run syntax:\nnbathy -W %d -i %s -x '%d %d' -y '%d %d' -P %s -n %dx%d" % \
-                              (0, TMPXYZ, nn_w, nn_e, nn_n, nn_s, ALG, cols,rows))
-        grass.message("Done. Raster map <%s> created." % options['output'])
-
-    region()
-    initial_controls()
-    compute()
-    convert()
-    import_to_raster()
-        # TODO UPOZORNeNi: nevhodny typ pole, pouzivam cele cislo
-
-def cleanup():
-    if TMP:
-        os.remove(TMP)
-    if TMPXYZ:
-        os.remove(TMPXYZ)
-    if XYZout:
-        os.remove(XYZout)
+    if (option['input']): 
+        obj = nnbathy_vector(options['input'])
+    else:
+        obj = nnbathy_file(options['file'])
+    
+    obj.compute()
+    obj.create_output()
 
 if __name__ == "__main__":
     options, flags = parser()
-    atexit.register(cleanup)
+    # atexit.register(cleanup)
     main()
