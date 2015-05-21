2ac340455411646b3dbf3b9e9ecdbd20ddff25ba
Martin Landa <landa.martin@gmail.com>
2015-05-21 13:07:35 +0200

    remove duplicated headers

--- src/cgal/main.cpp
+++ src/cgal/main.cpp
@@ -17,14 +17,6 @@
 #include <cstdlib>
 #include <vector>
 
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Triangulation_2.h>
-#include <CGAL/Delaunay_triangulation_2.h>
-
-#include <CGAL/Interpolation_traits_2.h>
-#include <CGAL/natural_neighbor_coordinates_2.h>
-#include <CGAL/interpolation_functions.h>
-
 extern "C" {
 #include <grass/vector.h>
 #include <grass/glocale.h>
--- src/cgal/write.cpp
+++ src/cgal/write.cpp
@@ -1,11 +1,3 @@
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Triangulation_2.h>
-#include <CGAL/Delaunay_triangulation_2.h>
-
-#include <CGAL/Interpolation_traits_2.h>
-#include <CGAL/natural_neighbor_coordinates_2.h>
-#include <CGAL/interpolation_functions.h>
-
 extern "C" {
 #include <grass/vector.h>
 #include <grass/glocale.h>
